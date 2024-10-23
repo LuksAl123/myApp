f4adb8854ec509863aad82e23110eaa0d8b4b67a
LuksAl123 <lucasalprogrammer@gmail.com>
2024-10-23 17:57:37 -0300

    commit 4 - List Fully Implemmented. Other pages were implemmented as well

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,9 +1,9 @@
-import { IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import { Redirect, Route } from 'react-router';
 import List from './List';
 import Settings from './Settings';
-import { homeOutline, newspaperOutline } from 'ionicons/icons';
+import { homeOutline, logOutOutline, newspaperOutline } from 'ionicons/icons';
 
 const Menu: React.FC = () => {
     const paths = [
@@ -23,12 +23,19 @@ const Menu: React.FC = () => {
                     <IonContent>
                         {paths.map((item, index) => (
                             <IonMenuToggle key={index} autoHide={false}>
-                                <IonItem detail={false} routerLink={item.url}>
+                                <IonItem detail={false} routerLink={item.url} routerDirection="none">
                                     <IonIcon slot="start" icon={item.icon} />
                                     {item.name}
                                 </IonItem>
                             </IonMenuToggle>
                         ))}
+
+                        <IonMenuToggle autoHide={false}>
+                                <IonButton expand="full" routerLink="/" routerDirection="root">
+                                    <IonIcon slot="start" icon={logOutOutline} />
+                                    Logout
+                                </IonButton>
+                        </IonMenuToggle>
                     </IonContent>
                 </IonMenu>
 
